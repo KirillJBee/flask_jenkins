b3c3a87e18aac9cc51854e1848242369d639d19d
KirillJBee <jbeework@gmail.com>
2024-02-29 18:52:33 +0300

    fix

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -48,8 +48,8 @@ pipeline {
                     sh 'docker run -d --rm -p 8000:8000 kirilljbee/testfluskapp:test'
                     sh 'ping -c 5 localhost'
                     sh 'curl http://localhost:8000'
-                    docker.image('kirilljbee/testfluskapp:test').tag(${env.BUILD_NUMBER})
-                    docker.image('kirilljbee/testfluskapp:test').push(${env.BUILD_NUMBER})
+                    docker.image('kirilljbee/testfluskapp:test').tag(prod)
+                    docker.image('kirilljbee/testfluskapp:test').push(prod)
                     sh 'docker stop $(docker ps -a -q)'
                     sh 'docker system prune -af'
                     cleanWs()
