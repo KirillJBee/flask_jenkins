17fd7af5f4e1df6a66e884d5c81676b3f047439f
KirillJBee <jbeework@gmail.com>
2024-03-03 13:10:19 +0300

    add

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -4,6 +4,7 @@ pipeline {
     environment {
         DOCKERHUB_CREDENTIALS = credentials('kirilljbee_dockerhub')
         NAME_IMAGE_DEV = 'kirilljbee/testfluskapp:dev'
+        TAG_IMAGE_PROD = 'prod'
 
     }
 
@@ -15,80 +16,80 @@ pipeline {
             }   
             
             steps {
-                sh 'docker build -t $NAME_IMAGE_DEV .'    
+                sh 'docker build -t ${NAME_IMAGE_DEV} .'    
             }
         }
 
-        stage('push devimage') {
-            agent { label 'awsssh'} 
+        // stage('push devimage') {
+        //     agent { label 'awsssh'} 
 
-            steps {
-                sh 'echo $DOCKERHUB_CREDENTIALS_PSW | docker login -u $DOCKERHUB_CREDENTIALS_USR --password-stdin'
-                sh 'docker push kirilljbee/testfluskapp:dev'
-                //sh 'docker stop $(docker ps -a -q)'
-                sh 'docker system prune -af'
-                cleanWs()
-                    dir("${env.WORKSPACE}@tmp") {
-                        deleteDir()
-                    }
-                     dir("${env.WORKSPACE}@script") {
-                        deleteDir()
-                    }
-                    dir("${env.WORKSPACE}@script@tmp") {
-                        deleteDir()
-                    }
-            }
-        }  
+        //     steps {
+        //         sh 'echo $DOCKERHUB_CREDENTIALS_PSW | docker login -u $DOCKERHUB_CREDENTIALS_USR --password-stdin'
+        //         sh 'docker push ${NAME_IMAGE_DEV}'
+        //         // sh 'docker stop '
+        //         // sh 'docker system prune -af'
+        //         cleanWs()
+        //             dir("${env.WORKSPACE}@tmp") {
+        //                 deleteDir()
+        //             }
+        //              dir("${env.WORKSPACE}@script") {
+        //                 deleteDir()
+        //             }
+        //             dir("${env.WORKSPACE}@script@tmp") {
+        //                 deleteDir()
+        //             }
+        //     }
+        // }  
         
-        stage('test devimage & push prodimage') {
-            agent { label 'PQHssh'} 
+        // stage('test devimage & push prodimage') {
+        //     agent { label 'PQHssh'} 
 
-            steps {
-                script {
-                    sh 'echo $DOCKERHUB_CREDENTIALS_PSW | docker login -u $DOCKERHUB_CREDENTIALS_USR --password-stdin'
-                    sh 'docker pull kirilljbee/testfluskapp:dev'
-                    sh 'docker run -d --rm -p 8000:8000 kirilljbee/testfluskapp:dev'
-                    sh 'ping -c 5 localhost'
+        //     steps {
+        //         script {
+        //             sh 'echo $DOCKERHUB_CREDENTIALS_PSW | docker login -u $DOCKERHUB_CREDENTIALS_USR --password-stdin'
+        //             sh 'docker pull kirilljbee/testfluskapp:dev'
+        //             sh 'docker run -d --rm -p 8000:8000 kirilljbee/testfluskapp:dev'
+        //             sh 'ping -c 5 localhost'
 
-                    sh 'curl http://localhost:8000'
+        //             sh 'curl http://localhost:8000'
 
-                    //docker.image('kirilljbee/testfluskapp:test').tag("${BUILD_NUMBER}")
-                    //docker.image('kirilljbee/testfluskapp:test').push("${BUILD_NUMBER}")
+        //             //docker.image('kirilljbee/testfluskapp:test').tag("${BUILD_NUMBER}")
+        //             //docker.image('kirilljbee/testfluskapp:test').push("${BUILD_NUMBER}")
 
-                    docker.image('kirilljbee/testfluskapp:dev').tag("prod")
-                    docker.image('kirilljbee/testfluskapp:dev').push("prod")
+        //             docker.image('kirilljbee/testfluskapp:dev').tag("prod")
+        //             docker.image('kirilljbee/testfluskapp:dev').push("prod")
 
-                    sh 'docker stop $(docker ps -aq)'
-                    //!!!!!!
-                    sh 'docker system prune -af'
+        //             sh 'docker stop $(docker ps -aq)'
+        //             //!!!!!!
+        //             sh 'docker system prune -af'
 
-                }
-            } 
-        }   
+        //         }
+        //     } 
+        // }   
         
-        stage('deploy production') {
-            agent { label 'PQHssh'}
+        // stage('deploy production') {
+        //     agent { label 'PQHssh'}
             
-            input {
-                    message "Ready to deploy?"
-                    ok "Yes"
-                }
-            steps {
-                sh 'ansible-playbook playbook.yml -i hosts.ini'
-                //sh 'ansible all -i hosts.ini -m ping'
-                //sh 'ansible-playbook playbook.yml'
-                cleanWs()
-                    dir("${env.WORKSPACE}@tmp") {
-                        deleteDir()
-                    }
-                     dir("${env.WORKSPACE}@script") {
-                         deleteDir()
-                    }
-                    dir("${env.WORKSPACE}@script@tmp") {
-                        deleteDir()
-                    }
-            }
-        }
+        //     input {
+        //             message "Ready to deploy?"
+        //             ok "Yes"
+        //         }
+        //     steps {
+        //         sh 'ansible-playbook playbook.yml -i hosts.ini'
+        //         //sh 'ansible all -i hosts.ini -m ping'
+        //         //sh 'ansible-playbook playbook.yml'
+        //         cleanWs()
+        //             dir("${env.WORKSPACE}@tmp") {
+        //                 deleteDir()
+        //             }
+        //              dir("${env.WORKSPACE}@script") {
+        //                  deleteDir()
+        //             }
+        //             dir("${env.WORKSPACE}@script@tmp") {
+        //                 deleteDir()
+        //             }
+        //     }
+        // }
     }
 
     post { 
