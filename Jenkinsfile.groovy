9722fea77c38ffa2a436e680d02021b500cc4b44
KirillJBee <Jbeework@gmail.com>
2024-03-04 13:11:14 +0300

    fix

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -4,10 +4,11 @@ pipeline {
     environment {
         NAME_PROJECT = 'testfluskapp'
         DOCKERHUB_CREDENTIALS = credentials('kirilljbee_dockerhub')
+        KEY_PROD = credentials ('passprodkey')
         NAME_IMAGE_DEV = 'kirilljbee/testfluskapp:dev'
         NAME_CONTAINER_DEV = 'testfluskapp_dev'
         TAG_IMAGE_PROD = 'prod'
-        KEY_PROD = credentials ('passprodkey')
+        
     }
 
     stages {
