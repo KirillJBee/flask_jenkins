0e7f641e809d58a4d6199e96cee1b78384f37112
KirillJBee <Jbeework@gmail.com>
2024-03-04 13:55:52 +0300

    fix

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -4,7 +4,7 @@ pipeline {
     environment {
         NAME_PROJECT = 'testfluskapp'
         DOCKERHUB_CREDENTIALS = credentials('kirilljbee_dockerhub')
-        //KEY_PROD = credentials ('passprodkey')
+        PROD_CREDENTIALS = credentials('passprodkey')
         NAME_IMAGE_DEV = 'kirilljbee/testfluskapp:dev'
         NAME_CONTAINER_DEV = 'testfluskapp_dev'
         TAG_IMAGE_PROD = 'prod'
@@ -68,7 +68,7 @@ pipeline {
             //     }
 
             steps {
-                sh 'ansible all -i inventory -m ping --connection-password-file $KEY_PROD'
+                sh 'ansible all -i inventory -m ping --connection-password-file $PROD_CREDENTIALS'
              
                 //sh 'ansible-playbook playbook.yml'
                 
