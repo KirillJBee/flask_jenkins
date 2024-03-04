accf61ede1a4f749288a127f551d7428e7cc2242
KirillJBee <jbeework@gmail.com>
2024-03-04 19:17:48 +0300

    add

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -3,8 +3,8 @@ pipeline {
 
     environment {
         NAME_PROJECT = 'testfluskapp'
-        DOCKERHUB_CREDENTIALS = credentials('kirilljbee_dockerhub')
-        PROD_CREDENTIALS = credentials('key_to_prod_server')
+        //DOCKERHUB_CREDENTIALS = credentials('kirilljbee_dockerhub')
+        PROD_CREDENTIALS = credentials('privatekeytxt')
         NAME_IMAGE_DEV = 'kirilljbee/testfluskapp:dev'
         NAME_CONTAINER_DEV = 'testfluskapp_dev'
         TAG_IMAGE_PROD = 'prod'
