9d8d67f84d59fefb23872e022a32c6fcea82ed6f
KirillJBee <jbeework@gmail.com>
2024-03-11 21:27:56 +0300

    add

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-	return 'Hello, Ansible :-)!\n'
+	return 'Hello, Jenkins :-)!\n'
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=8000)
